80428780a8832f25e6dee04d9c28b6a55033b93e
 NoraBourouis <bourouisnora@gmail.com>
2019-01-30 18:51:42 +0100

    Manque les comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do
+  lecture = Lecture.create!(title: Faker::GameOfThrones.house)
+end
+
+10.times do
+  student = Student.create!(name: Faker::GameOfThrones.character, lecture_id: rand(1..11))
+end
